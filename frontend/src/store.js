f28ba76388b3b7392e4a3f44a9c3cc8bbef226c8
jquick5 <jay.quick79@gmail.com>
2021-06-25 15:14:51 -0700

    Create Order Completed

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,43 @@ import {
 	productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import {
+	userDetailsReducer,
+	userLoginReducer,
+	userRegesterReducer,
+	userUpdateProfileReducer,
+} from './reducers/userReducers';
+import { orderCreateReducer } from './reducers/orderReducers';
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
+	userLogin: userLoginReducer,
+	userRegister: userRegesterReducer,
+	userDetails: userDetailsReducer,
+	userUpdateProfile: userUpdateProfileReducer,
+	orderCreate: orderCreateReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
 	? JSON.parse(localStorage.getItem('cartItems'))
 	: [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+	? JSON.parse(localStorage.getItem('userInfo'))
+	: null;
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+	? JSON.parse(localStorage.getItem('shippingAddress'))
+	: {};
+
 const initialState = {
-	cart: { cartItems: cartItemsFromStorage },
+	cart: {
+		cartItems: cartItemsFromStorage,
+		shippingAddress: shippingAddressFromStorage,
+	},
+	userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
